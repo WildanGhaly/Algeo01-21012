c340807a41fe914657467a71026b18b1a4596751
WildanGhaly <16521502@std.stei.itb.ac.id>
2022-09-19 19:05:57 +0700

    NewFile Gauss

--- src/Gauss_Jordan.java
+++ src/Gauss_Jordan.java
@@ -1,10 +1,8 @@
-// Berisikan fungsi untuk menyelesaikan SPL dengan metode Gauss
-
+// Berisikan fungsi untuk menyelesaikan SPL dengan metode Gauss Jordan
 
 import java.util.Scanner;
 
-
-public class Gauss {
+public class Gauss_Jordan {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in); //Membuka fungsi scanner
 
@@ -13,14 +11,6 @@ public class Gauss {
 
         scan.close(); //Menutup fungsi scanner
     }
-    
 }
-
-
-
-
-
-
-
 
 
---
+++ src/Menu.java
@@ -0,0 +1,12 @@
+public class Menu {
+    public static void main(String[] args) {
+        System.out.println("Menu");
+        System.out.println("1. Sistem Persamaan Linier");
+        System.out.println("2. Determinan");
+        System.out.println("3. Matriks balikan");
+        System.out.println("4. Interpolasi Polinom");
+        System.out.println("5. Interpolasi Bicubic");
+        System.out.println("6. Regresi linier berganda");
+        System.out.println("7. Keluar");
+    }
+}
