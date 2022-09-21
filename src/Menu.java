fd8e6fb12b4069241d0ad41206d6bec71402b539
WildanGhaly <16521502@std.stei.itb.ac.id>
2022-09-21 14:27:15 +0700

    update menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,6 +1,11 @@
 
 public class Menu {
+    /* Class ini berisi beberapa method untuk bagian MENU
+     * Class Menu ini HANYA memiliki method yang dapat digunakan untuk DITAMPILKAN ke LAYAR
+     */
+
     public void tampilanMenu() {
+        /* Menampilkan daftar MENU UTAMA pada layar */
         System.out.println("Menu");
         System.out.println("1. Sistem Persamaan Linier");
         System.out.println("2. Determinan");
@@ -11,6 +16,7 @@ public class Menu {
         System.out.println("7. Keluar");
     }
     public void tampilanSubMenu1() {
+        /* Menampilkan daftar SUB MENU PERTAMA pada layar */
         System.out.println("Sub menu");
         System.out.println("1. Metode eliminasi Gauss");
         System.out.println("2. Metode eliminasi Gauss-Jordan");
@@ -19,6 +25,7 @@ public class Menu {
     }
 
     public void tampilanSubMenu2() {
+        /* Menampilkan daftar SUB MENU KEDUA pada layar */
         System.out.println("Sub menu");
         System.out.println("1. Metode eliminasi Gauss");
         System.out.println("2. Metode eliminasi Gauss-Jordan");
@@ -26,6 +33,7 @@ public class Menu {
     }
 
     public void tampilanSubMenu3() {
+        /* Menampilkan daftar SUB MENU KETIGA pada layar */
         System.out.println("Sub menu");
         System.out.println("1. Metode adjoin");
         System.out.println("2. Metode Operasi Baris Elementer");
