462817c7947899634b5d45aa147ab9926f3fc935
WildanGhaly <16521502@std.stei.itb.ac.id>
2022-09-23 23:25:22 +0700

    bersihin file

--- src/Gauss_Jordan.java
+++ src/Gauss_Jordan.java
@@ -1,16 +1,17 @@
 
 
-import java.util.Scanner;
+
 
 public class Gauss_Jordan {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in); //Membuka fungsi scanner
 
 
 
 
-        scan.close(); //Menutup fungsi scanner
-    }
+
+
+
+
+
 }
 
 
